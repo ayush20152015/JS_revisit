dcafeb39d40035bcc7901106661cfa231c7d313f
ayush choudhary <ayushchoudhary@ayush-macbook.local>
2026-03-17 23:00:59 +0530

    second

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
-console.log("hello");
-console.log("hi");
+
+
+// datatypes
+const c=4; // cant be redeclared or changed
+let a="hello";
+var b=2; // not a good practice
+id=4; // not a good practice
+
+ let rand; // undefined
+
+console.log(c); // output one at a time
+console.table([a,b,c,id,rand]) // output multiple variable at a time
+
+
+// dont use var because of issues in blocks and functional space
+
+ 
