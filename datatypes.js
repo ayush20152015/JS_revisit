fecffd0bc7e97c6420c9fd737be1738397dff0f6
ayush choudhary <ayushchoudhary@ayush-macbook.local>
2026-03-18 14:04:27 +0530

    datatypesadded

---
+++ datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; // newer version of JS
+// alert(3+3) // gives error in node
+console.log(3+3);
+console.log("ayush");
+
+let name = "ayush";
+let age=18;
+let isLoggedIn=false;
+
+// number => (1<<53)
+// bigint
+// string
+// boolean=> true/false
+// null=> standalone value
+
+let state=null;
+console.log(state); // gives null
+
+//. undefined
+// symbols => unique
+
+console.log(typeof(null)); // type of object
+
+console.log(typeof(age)); // number type
+
+let value = Number(age); // conversion
+
+let missing = "33a";
+
+value = Number(missing); // failed conversion as NaN
+
+console.log(typeof(value));
+
+console.log(value);
+
+// conversion is also discussed
+
+
+
+
