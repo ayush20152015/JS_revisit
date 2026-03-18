0c896fed1200da644c9c66abb60c6b5014ab3425
ayush20152015 <atyud2015@gmail.com>
2026-03-18 14:13:52 +0530

    datatypes added

---
+++ operations.js
@@ -0,0 +1,13 @@
+// operations
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*2);
+console.log(3/2);
+
+console.log(3>=3);
+console.log(3==3);
+console.log(3===3); // strict comparison
+console.log(3==="3"); // gives false
+console.log(2**3); // power function
+console.log(3>=null);
